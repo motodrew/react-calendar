02fd6e0e6a8cae6b408fc32abed5b0e18526eca8
Drew Casebeer <dcasebeer@trident-it.com>
2019-02-06 09:47:22 -0600

    Added Events Component

--- src/shared/utils.js
+++ src/shared/utils.js
@@ -6,8 +6,7 @@ import { getRange } from './dates';
  *
  * @param {Function[]} functions
  */
-export const mergeFunctions = (...functions) => (...args) => functions
-  .filter(Boolean).forEach(f => f(...args));
+export const mergeFunctions = (...functions) => (...args) => functions.filter(Boolean).forEach(f => f(...args));
 
 /**
  * Calls a function, if it's defined, with specified arguments
@@ -15,25 +14,18 @@ export const mergeFunctions = (...functions) => (...args) => functions
  * @param {Object} args
  */
 export const callIfDefined = (fn, ...args) => {
-  if (fn && typeof fn === 'function') {
-    fn(...args);
-  }
+	if (fn && typeof fn === 'function') {
+		fn(...args);
+	}
 };
 
-export const isValueWithinRange = (value, range) => (
-  range[0] <= value
-  && range[1] >= value
-);
+export const isValueWithinRange = (value, range) => range[0] <= value && range[1] >= value;
 
-export const isRangeWithinRange = (greaterRange, smallerRange) => (
-  greaterRange[0] <= smallerRange[0]
-  && greaterRange[1] >= smallerRange[1]
-);
+export const isRangeWithinRange = (greaterRange, smallerRange) =>
+	greaterRange[0] <= smallerRange[0] && greaterRange[1] >= smallerRange[1];
 
-export const doRangesOverlap = (range1, range2) => (
-  isValueWithinRange(range1[0], range2)
-  || isValueWithinRange(range1[1], range2)
-);
+export const doRangesOverlap = (range1, range2) =>
+	isValueWithinRange(range1[0], range2) || isValueWithinRange(range1[1], range2);
 
 /**
  * Returns a value no smaller than min and no larger than max.
@@ -43,81 +35,78 @@ export const doRangesOverlap = (range1, range2) => (
  * @param {*} max Maximum return value.
  */
 export const between = (value, min, max) => {
-  if (min && min > value) {
-    return min;
-  }
-  if (max && max < value) {
-    return max;
-  }
-  return value;
+	if (min && min > value) {
+		return min;
+	}
+	if (max && max < value) {
+		return max;
+	}
+	return value;
 };
 
-export const getTileClasses = ({
-  value, valueType, date, dateType, hover,
-} = {}) => {
-  const className = 'react-calendar__tile';
-  const classes = [className];
+export const getTileClasses = ({ value, valueType, date, dateType, hover, events } = {}) => {
+	const className = 'react-calendar__tile';
+	const classes = [className];
 
-  if (!date) {
-    return classes;
-  }
+	if (!date) {
+		return classes;
+	}
 
-  if (!(date instanceof Array) && !dateType) {
-    throw new Error('getTileClasses(): Unable to get tile activity classes because one or more required arguments were not passed.');
-  }
+	if (!(date instanceof Array) && !dateType) {
+		throw new Error(
+			'getTileClasses(): Unable to get tile activity classes because one or more required arguments were not passed.'
+		);
+	}
 
-  const now = new Date();
-  const dateRange = date instanceof Array ? date : getRange(dateType, date);
+	const now = new Date();
+	const dateRange = date instanceof Array ? date : getRange(dateType, date);
 
-  if (isValueWithinRange(now, dateRange)) {
-    classes.push(`${className}--now`);
-  }
+	if (isValueWithinRange(now, dateRange)) {
+		classes.push(`${className}--now`);
+	}
 
-  if (!value) {
-    return classes;
-  }
+	if (!value) {
+		return classes;
+	}
 
-  if (!(value instanceof Array) && !valueType) {
-    throw new Error('getTileClasses(): Unable to get tile activity classes because one or more required arguments were not passed.');
-  }
+	if (!(value instanceof Array) && !valueType) {
+		throw new Error(
+			'getTileClasses(): Unable to get tile activity classes because one or more required arguments were not passed.'
+		);
+	}
 
-  const valueRange = value instanceof Array ? value : getRange(valueType, value);
+	const valueRange = value instanceof Array ? value : getRange(valueType, value);
 
-  if (isRangeWithinRange(valueRange, dateRange)) {
-    classes.push(`${className}--active`);
-  } else if (doRangesOverlap(valueRange, dateRange)) {
-    classes.push(`${className}--hasActive`);
-  } else if (
-    hover && (
-      // Date before value
-      (
-        dateRange[1] < valueRange[0]
-        && isRangeWithinRange([hover, valueRange[0]], dateRange)
-      )
-      // Date after value
-      || (
-        dateRange[0] > valueRange[1]
-        && isRangeWithinRange([valueRange[1], hover], dateRange)
-      )
-    )
-  ) {
-    classes.push(`${className}--hover`);
-  }
+	if (isRangeWithinRange(valueRange, dateRange)) {
+		classes.push(`${className}--active`);
+	} else if (doRangesOverlap(valueRange, dateRange)) {
+		classes.push(`${className}--hasActive`);
+	} else if (
+		hover &&
+		// Date before value
+		((dateRange[1] < valueRange[0] && isRangeWithinRange([hover, valueRange[0]], dateRange)) ||
+			// Date after value
+			(dateRange[0] > valueRange[1] && isRangeWithinRange([valueRange[1], hover], dateRange)))
+	) {
+		classes.push(`${className}--hover`);
+	}
 
-  const isRangeStart = isValueWithinRange(valueRange[0], dateRange);
-  const isRangeEnd = isValueWithinRange(valueRange[1], dateRange);
+	const isRangeStart = isValueWithinRange(valueRange[0], dateRange);
+	const isRangeEnd = isValueWithinRange(valueRange[1], dateRange);
 
-  if (isRangeStart) {
-    classes.push(`${className}--rangeStart`);
-  }
+	if (isRangeStart) {
+		classes.push(`${className}--rangeStart`);
+	}
 
-  if (isRangeEnd) {
-    classes.push(`${className}--rangeEnd`);
-  }
+	if (isRangeEnd) {
+		classes.push(`${className}--rangeEnd`);
+	}
 
-  if (isRangeStart && isRangeEnd) {
-    classes.push(`${className}--rangeBothEnds`);
-  }
+	if (isRangeStart && isRangeEnd) {
+		classes.push(`${className}--rangeBothEnds`);
+	}
 
-  return classes;
+	console.log(events);
+
+	return classes;
 };
